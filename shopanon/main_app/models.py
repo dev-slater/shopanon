5a60d63463d78bd2aa681326d6cc26eceb7db798
Matthew Slater <slaterm100@gmail.com>
2021-11-13 17:03:24 -0500

    change admin settings

--- shopanon/main_app/models.py
+++ shopanon/main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+
     
 class Product(models.Model):
     name = models.CharField(max_length=50)
