2c2db8efe4163b1a914f7775dd0e4a3a0f0085b2
Matthew Slater <slaterm100@gmail.com>
2021-11-18 00:33:54 -0500

    jwt auth postman test success

--- shopanon/shopanon/urls.py
+++ shopanon/shopanon/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('main_app.urls','main_app'), namespace='main_app')),
     path('api/', include(('api.urls','api'), namespace='api')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api-auth',include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
