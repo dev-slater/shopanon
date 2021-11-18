3a7f428b6e532f1e1676a0aaa33672ccbca66d84
Matthew Slater <slaterm100@gmail.com>
2021-11-17 20:45:44 -0500

    add simple jwt

--- shopanon/shopanon/urls.py
+++ shopanon/shopanon/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('main_app.urls','main_app'), namespace='main_app')),
     path('api/', include(('api.urls','api'), namespace='api')),
+    path('api-auth',include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
